2c9a1469e5d072c9461a5b4e95ee698ce63e81c6
TalhaProgrammer92 <talha.code92@gmail.com>
2025-02-13 21:28:20 +0500

    a

---
+++ Arrays/print sub arrays.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+    void printAllSubArrays(int array[], int size)
+    {
+        // Loop through all subarrays
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = i; j < size; j++)
+            {
+                // Print the subarray
+                for (int k = i; k <= j; k++)
+                    cout << array[k] << " ";
+                cout << endl;
+            }
+        }
+    }
+};
+
+int main()
+{
+    // Test the algorithm
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    Solution().printAllSubArrays(arr, size);
+}
