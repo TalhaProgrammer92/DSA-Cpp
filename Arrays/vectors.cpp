78ed35e39046bf8e7b9701715fe9caaac1af4ead
TalhaProgrammer92 <talha.code92@gmail.com>
2025-02-13 19:06:06 +0500

    vector example source file added

---
+++ Arrays/vectors.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    // Create an empty vector of integers
+    vector<int> my_vector;
+
+    // Add elements to the vector
+    my_vector.push_back(10);
+    my_vector.push_back(20);
+    my_vector.push_back(30);
+    my_vector.push_back(40);
+    
+    // Print the vector
+    cout << "Vector elements: ";
+    for (const int element : my_vector)     // For-each loop
+        cout << element << " ";
+}
