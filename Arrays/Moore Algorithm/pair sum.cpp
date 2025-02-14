587cae660e52d197fbff9269ce603e6734e8cb84
TalhaProgrammer92 <talha.code92@gmail.com>
2025-02-14 12:18:24 +0500

    pair sum program

---
+++ Arrays/Moore Algorithm/pair sum.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// ? Find out the pair of given array that's pair sum is equal to the given targer by using brute force approach.
+// TODO: Array: {2, 7, 11, 15}, Target: 9
+
+class Solution
+{
+public:
+    void printPairs(vector<int> &array, int target)
+    {
+        for (int i = 0; i < array.size(); i++)
+        {
+            for (int j = i + 1; j < array.size(); j++)
+            {
+                if (array[i] + array[j] == target)
+                {
+                    cout << "Sum of the pair (" << array[i] << ", " << array[j] << ") is " << target << endl;
+                    return;
+                }
+            }
+        }
+        cout << "No pairs found with the given target." << endl;
+    }
+};
+
+int main()
+{
+    vector<int> array = {2, 7, 11, 15};
+    Solution().printPairs(array, 9);
+}
