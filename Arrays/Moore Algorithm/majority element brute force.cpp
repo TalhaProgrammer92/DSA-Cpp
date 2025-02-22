b09755d8b03aa2dbcf33305707bdbccb47b6ba68
Talha Ahmad <talha.code92@gmail.com>
2025-02-22 11:13:04 +0500

    Update Moore

--- Arrays/Moore Algorithm/majority element brute force.cpp
+++ Arrays/Moore Algorithm/majority element brute force.cpp
@@ -6,25 +6,29 @@ using namespace std;
 // ? Find out majority element (appears more than n/2 times) in an array by using Brute Force Approach.
 // TODO: Array = {1, 2, 2, 1, 1}, Majority Element = 1
 
-class Solution{
+class Solution
+{
 public:
-    int findMajorityElement(vector<int>& nums){
+    int findMajorityElement(vector<int> &nums)
+    {
         int count, limit = nums.size() / 2;
 
         //* Iterate through each element in the array.
-        for(int i = 0; i < nums.size(); i++){
+        for (int i = 0; i < nums.size(); i++)
+        {
             count = 1;
-            for(int j = i + 1; j < nums.size(); j++){
+            for (int j = i + 1; j < nums.size(); j++)
+            {
                 //* If current element is same as the next element, increment the count.
-                if(nums[i] == nums[j])
+                if (nums[i] == nums[j])
                     count++;
                 //* If count reaches the limit (n/2), return the current element as majority element.
-                if(count > limit)
+                if (count > limit)
                     return nums[i];
             }
             count = 0;
         }
-        return -1;  //* Majority element not found in the array.
+        return -1; //* Majority element not found in the array.
     }
 };
 
@@ -38,6 +42,6 @@ int main()
     Output:
 
     1
- 
+
     Time Complexity: O(n^2)
 */
---
+++ Arrays/Moore Algorithm/majority element sorting.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// ? Find out majority element (appears more than n/2 times) in an array by sorting the array.
+// TODO: Array = {1, 2, 2, 1, 1}, Majority Element = 1
+
+class Solution
+{
+public:
+    int findMajorityElement(vector<int> &nums)
+    {
+        int count = 1, limit = nums.size() / 2;
+        sort(nums.begin(), nums.end()); //* Sort the array.
+
+        // Count the number of times the current element appears in the array.
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] == nums[i + 1])
+            {
+                count++;
+            }
+            else
+            {
+                count = 1;
+            }
+
+            // If count reaches the limit (n/2), return the current element as majority element.
+            if (count > limit)
+            {
+                return nums[i];
+            }
+        }
+    }
+};
+
+int main()
+{
+    vector<int> numbers = {1, 2, 2, 1, 1};
+    cout << Solution().findMajorityElement(numbers) << endl;
+}
+
+/*
+    Output:
+
+    1
+
+    Time Complexity:
+     O(n.log(n)) - Sorting the array.
+     O(n) - Iterating through the array to find the majority element.
+    => O(n.log(n)) + O(n) = O(n.log(n))
+*/
