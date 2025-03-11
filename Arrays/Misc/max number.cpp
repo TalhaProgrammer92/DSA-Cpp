ce35665b8be485cd76b81b64e0b573988e9fd8fb
TalhaProgrammer92 <talha.code92@gmail.com>
2025-03-12 04:57:16 +0500

    Maximum number in an array

---
+++ Arrays/Misc/max number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //* Array/Variables
+    int array[] = {2, 1, 5, 8, 4, 6, 9, 7};
+    int size = sizeof(array) / sizeof(int), max = array[0];
+
+    //* Algorithm
+    for (int i = 1; i < size; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+
+    //* Display Result
+    cout << "Maximum number is " << max << endl;
+}
