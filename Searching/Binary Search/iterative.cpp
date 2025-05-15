ea62a90de9c172a4b968b26fd673dec5b7c04516
TalhaProgrammer92 <talha.code92@gmail.com>
2025-05-15 09:31:24 +0500

    .

--- Searching/Binary Search/iterative.cpp
+++ Searching/Binary Search/iterative.cpp
@@ -4,24 +4,30 @@ using namespace std;
 
 int binary_search(int array[], int size, int &target)
 {
+    //* If the array is not NULL
     if (array != NULL)
     {
+        //* Initialize start and end indexes
         int start = 0, end = size - 1;
 
+        //* Searching
         while (start <= end)
         {
+            //* Initialize mid index
             int mid = (start + end) / 2;
 
+            //* If the number at mid index is less than target
             if (array[mid] < target)
                 start = mid + 1;
-            
+
+            //* If the number at mid index is greater than target
             else if (array[mid] > target)
                 end = mid - 1;
-            
+
+            //* If the number at mid index is equal to target
             else if (array[mid] == target)
                 return mid;
         }
-        
     }
 
     return -1;
