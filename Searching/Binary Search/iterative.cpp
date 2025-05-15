8083ca9d7ab814f4251d0a19c238fdcd605fb10c
TalhaProgrammer92 <talha.code92@gmail.com>
2025-05-15 09:29:54 +0500

    .

--- Searching/Binary Search/iterative.cpp
+++ Searching/Binary Search/iterative.cpp
@@ -2,7 +2,36 @@
 
 using namespace std;
 
+int binary_search(int array[], int size, int &target)
+{
+    if (array != NULL)
+    {
+        int start = 0, end = size - 1;
+
+        while (start <= end)
+        {
+            int mid = (start + end) / 2;
+
+            if (array[mid] < target)
+                start = mid + 1;
+            
+            else if (array[mid] > target)
+                end = mid - 1;
+            
+            else if (array[mid] == target)
+                return mid;
+        }
+        
+    }
+
+    return -1;
+}
+
 int main()
 {
-    
+    //! Binary Search
+    int target = 12, array[] = {-1, 0, 3, 4, 5, 9, 12}, index = binary_search(array, sizeof(array) / sizeof(array[0]), target);
+
+    //! Display output
+    cout << "The target " << target << " is located at index " << index << endl;
 }
