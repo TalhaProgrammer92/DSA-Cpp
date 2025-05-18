d56302125fa8da9dd1923808dbec6422a0caa569
TalhaProgrammer92 <talha.code92@gmail.com>
2025-05-18 14:34:44 +0500

    .

---
+++ Arrays/Strings/Palindrome/program.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+//? This is my own approach ;-)
+
+int main() {
+	system("cls");
+
+	string s = "racecar";
+	int c = s[0];
+
+	for (int i = 1; i < s.length(); i++)
+		c ^= s[i];
+	
+	switch (s.length() % 2)
+	{
+	case 0:		// Even
+		(c == 0) ? cout << "It's a palindrome" : cout << "It's not a palindrome";
+		break;
+	
+	case 1:		// Odd
+		(c == s[(int) (s.length() / 2)]) ? cout << "It's a palindrome" : cout << "It's not a palindrome";
+		break;
+	}
+
+	cout << endl;
+
+	return 0;
+}
