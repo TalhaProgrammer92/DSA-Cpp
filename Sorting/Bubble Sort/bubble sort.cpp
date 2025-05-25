c2a10f55a603d085510b398c177770091e418112
TalhaProgrammer92 <talha.code92@gmail.com>
2025-05-25 14:44:18 +0500

    .

--- Sorting/Bubble Sort/bubble sort.cpp
+++ Sorting/Bubble Sort/bubble sort.cpp
@@ -3,9 +3,8 @@ using namespace std;
 
 // Function to perform Bubble Sort
 void bubbleSort(int arr[], int n) {
-    // bool swapped;
     for (int i = 0; i < n - 1; i++) {
-        swapped = false;
+        bool swapped = false;
         for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 swap(arr[j], arr[j + 1]);
