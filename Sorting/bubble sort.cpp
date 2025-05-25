40f8fe7671955630f1c3aee31962dcf57c038f99
TalhaProgrammer92 <talha.code92@gmail.com>
2025-05-25 14:30:40 +0500

    .

--- Sorting/bubble sort.cpp
+++ Sorting/bubble sort.cpp
@@ -3,17 +3,17 @@ using namespace std;
 
 // Function to perform Bubble Sort
 void bubbleSort(int arr[], int n) {
-    bool swapped;
+    // bool swapped;
     for (int i = 0; i < n - 1; i++) {
-        swapped = false;
+        // swapped = false;
         for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 swap(arr[j], arr[j + 1]);
-                swapped = true;
+                // swapped = true;
             }
         }
         // If no swapping happened, the array is already sorted
-        if (!swapped) break;
+        // if (!swapped) break;
     }
 }
 
