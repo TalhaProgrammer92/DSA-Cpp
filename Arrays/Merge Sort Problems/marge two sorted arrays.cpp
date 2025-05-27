4faa2ec1ebd15c5d40ccd26ce50e7602d0324ecf
TalhaProgrammer92 <talha.code92@gmail.com>
2025-05-27 18:34:28 +0500

    .

--- Arrays/Merge Sort Problems/marge two sorted arrays.cpp
+++ Arrays/Merge Sort Problems/marge two sorted arrays.cpp
@@ -6,13 +6,17 @@ using namespace std;
 int main()
 {
 	// * Variables
-	vector<int> a = {1, 2, 3, 0, 0, 0}, b = {2, 5, 6};
+	// vector<int> a = {1, 2, 3, 0, 0, 0}, b = {2, 5, 6};
+	vector<int> a = {4, 5, 6, 0, 0, 0}, b = {1, 2, 3};
 	int m = 3, n = 3;
 
 	// * Algorithm
 	int index = m + n - 1, i = m - 1, j = n - 1;
-	while (i >= 0 && j >= 0)
-		a[index--] = (b[i] > a[i]) ? b[j--] : a[i--];
+	while (j >= 0)
+		if ( i >= 0)
+			a[index--] = (b[i] > a[i]) ? b[j--] : a[i--];
+		else
+			a[index--] = b[j--];
 
 	// * Print the array
 	for (int i = 0; i < m + n; i++)
