d4b75186968663eb3604073022e4a0754f40057f
TalhaProgrammer92 <talha.code92@gmail.com>
2025-05-27 18:12:34 +0500

    .

---
+++ Arrays/Merge Sort Problems/marge two sorted arrays.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	// * Variables
+	vector<int> a = {1, 2, 3, 0, 0, 0}, b = {2, 5, 6};
+	int m = 3, n = 3;
+
+	// * Algorithm
+	
+
+	return 0;
+}
