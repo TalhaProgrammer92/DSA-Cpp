f01a0f96f09ffe055d6a1898e087f4440292c96b
TalhaProgrammer92 <talha.code92@gmail.com>
2025-06-17 15:45:27 +0500

    .

--- Stack/Valid Paranthesis/program.cpp
+++ Stack/Valid Paranthesis/program.cpp
@@ -6,41 +6,46 @@ using namespace std;
 bool is_valid_parenthesis(string paren)
 {
 	// ? If has only one or no parenthesis
-	if (paren.length() <= 1) return false;
+	if (paren.length() <= 1)
+		return false;
 
 	// ? Check if opening and clossing parenthesis exist or not
 	stack<char> parenthesis;
 	for (char p : paren)
 	{
-		// ? Push opening parenthesis onto stack
+		// ? Push opening parenthesis onto stack - Opening bracket case
 		if (p == '(' || p == '[' || p == '{')
 			parenthesis.push(p);
-		// ? Pop recently added open parenthesis and check if its closing one exist in correct place
+		// ? Pop recently added open parenthesis and check if its closing one exist in correct place - Closing bracket case
 		else
 		{
+			// ! If closing bracket had no opening bracket
+			if (parenthesis.empty())
+				return false;
+
 			char last = parenthesis.top();
-			
+
 			switch (p)
 			{
 			case ')':
 				if (last != '(')
 					return false;
 				break;
-			
+
 			case ']':
 				if (last != '[')
 					return false;
 				break;
-			
+
 			case '}':
 				if (last != '{')
 					return false;
 				break;
-			
+
 			default:
 				return false;
 			}
-			
+
 			parenthesis.pop();
 		}
 	}
