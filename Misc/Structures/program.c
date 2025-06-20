edc1bdf4af63b8adfe296145901f149fe4170a51
TalhaProgrammer92 <talha.code92@gmail.com>
2025-06-20 15:44:57 +0500

    .

---
+++ Misc/Structures/program.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// ? Structure
+struct Student
+{
+	int roll, marks; char grade;
+};
+
+int main()
+{
+	struct Student s1, s2; // ! In C++ you do not need to write struct again when create object of a structure
+
+	s1.roll = 19;
+	s1.marks = 65;
+	s1.grade = 'A';
+
+	s2.roll = 15;
+	s2.marks = 41;
+	s2.grade = 'B';
+
+	struct Student *s = (struct Student *) malloc(sizeof(struct Student));
+
+	s->roll = 34;
+	s->marks = 75;
+	s->grade = 'A';
+
+	return 0;
+}
